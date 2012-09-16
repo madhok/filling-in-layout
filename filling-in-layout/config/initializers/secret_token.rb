8ad6ddc8802fe48036f87edcd95f272c2517c8d9
Madhok Shivaratre <gsmadhok@gmail.com>
2012-09-16 11:44:11 -0700

    Initail Commit
    Created a rails server for filling in layout

---
+++ filling-in-layout/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FillingInLayout::Application.config.secret_token = 'f01487a4039f25ee938c17c15b5703429463e597a8d2665b0f4353db40ec2dd597647c6dd4429fb02613b8578a84120f092d3ee3b5e32931bed8c2fc1a34ca5c'
---
+++ filling-in-layout/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FillingInLayout::Application.config.session_store :cookie_store, key: '_filling-in-layout_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FillingInLayout::Application.config.session_store :active_record_store
